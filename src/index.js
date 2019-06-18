1acfeb55bbcda32d5d0cae2b5eeff5d271ccc979
huangchucai <425205839@qq.com>
2019-06-18 21:34:24 +0800

    引入第三方模块

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 require('@babel/polyfill');
 
-require('./index.css');
-require('./test.styl');
-const a = require('./a.js');
-console.log(a);
-let fn = () => {
-    console.log(a.name);
-};
-fn();
 
-console.log([1, 2, 3].includes(2));
+console.log($);
+
